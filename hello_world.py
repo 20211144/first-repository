7610a706b887f00e3cae06c31e7c2e4e56ac3796
20211144 <gkskdml7419@gmail.com>
2023-09-14 11:09:15 +0900

    add print_KIT()

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,10 @@
 def print_hello():
     print("Hello World!!")
 
+def print_KIT():
+    print("kumoh national Institute of Technolohy.")
+
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
-    
+
